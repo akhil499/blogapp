d6ee2873d84ae5cb87c7f1274de3f6e6c8f815e2
Akhilesh K M <kmakhil294@gmail.com>
2023-10-18 15:20:04 +0530

    adding entities and repositories

---
+++ src/main/java/com/blogapp/blogapp/comments/CommentEntity.java
@@ -0,0 +1,41 @@
+package com.blogapp.blogapp.comments;
+
+import com.blogapp.blogapp.articles.ArticleEntity;
+import com.blogapp.blogapp.users.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import java.util.Date;
+
+@Entity(name = "comments")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Nullable
+    private String title;
+
+    @NonNull
+    private String body;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "articleId", nullable = false)
+    private ArticleEntity article;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId", nullable = false)
+    private UserEntity author;
+}
---
+++ src/main/java/com/blogapp/blogapp/users/UserEntity.java
@@ -0,0 +1,34 @@
+package com.blogapp.blogapp.users;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String username;
+
+    @Column(nullable = false)
+    @NonNull
+    private String email;
+
+    @Column(nullable = true)
+    @Nullable
+    private String bio;
+
+    private String img;
+}
